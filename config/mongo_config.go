3fe5fafb8c6634ea40de3b1183baadbb90425317
Alexis Trejo <marcoalexispt.02@gmail.com>
2025-08-13 13:59:32 -0600

    feat: added twilio go package and mongo package

---
+++ config/mongo_config.go
@@ -0,0 +1,49 @@
+package config
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+)
+
+var mongoClient *mongo.Client
+
+func InitMongo() *mongo.Client {
+	var uri string
+	if uri = os.Getenv("MONGODB_URI"); uri == "" {
+		log.Fatal("'MONGODB_URI' environment variable is not set. Closing Server.")
+	}
+
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
+	client, err := mongo.Connect(opts)
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+	var result bson.M
+	if err := client.Database("admin").RunCommand(context.TODO(), bson.D{{Key: "ping", Value: 1}}).Decode(&result); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
+
+	return client
+}
+
+func GetMongoClient() *mongo.Client {
+	if mongoClient == nil {
+		mongoClient = InitMongo()
+	}
+	return mongoClient
+}
---
+++ config/twilio_config.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"os"
+
+	"github.com/twilio/twilio-go"
+)
+
+var twilioClient *twilio.RestClient
+
+func InitTwilio() {
+	accountSid := os.Getenv("TWILIO_ACCOUNT_SID")
+	authToken := os.Getenv("TWILIO_AUTH_TOKEN")
+
+	if accountSid == "" || authToken == "" {
+		panic("Twilio credentials are not set in environment variables")
+	}
+
+	client := twilio.NewRestClientWithParams(twilio.ClientParams{
+		Username: accountSid,
+		Password: authToken,
+	})
+
+	twilioClient = client
+}
+
+func GetTwilioClient() *twilio.RestClient {
+	if twilioClient == nil {
+		InitTwilio()
+	}
+
+	return twilioClient
+}
